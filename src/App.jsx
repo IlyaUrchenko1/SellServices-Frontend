981f28fbf62a1a78752b1e6f58973e05701d8c4d
IlyaUrchenko1 <yurchenko0102@gmail.com>
2024-12-22 17:17:57 +0300

    Init

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+  return (
+    <div>
+      <input placeholder='Введите что-то'></input>
+      <input placeholder='Введите что-то'></input>
+      <input placeholder='Введите что-то'></input>
+      <input placeholder='Введите что-то'></input>
+    </div>
+  )
+}
+
+export default App
