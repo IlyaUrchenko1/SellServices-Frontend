2bfd5b57526744afc894bf9af4f3e36953af8527
IlyaUrchenko1 <yurchenko0102@gmail.com>
2024-12-25 21:18:12 +0300

    Try fix bag with netify

--- src/App.jsx
+++ src/App.jsx
@@ -3,21 +3,19 @@ import './App.css'
 import Create from './components/Create/Create'
 import Search from './components/Search/Search'
 import Update from './components/Update/Update'
-import Default from './components/Default/Default'
 
 function App() {
   return (
     <Router>
       <nav>
         <ul>
-          <li><Link to="/create">Создание</Link></li>
+          <li><Link to="/">Создание</Link></li>
           <li><Link to="/update">Обновление</Link></li>
           <li><Link to="/search">Поиск</Link></li>
         </ul>
       </nav>
       <Routes>
-        <Route path="/" element={<Default />} />
-        <Route path="/create" element={<Create />} />
+        <Route path="/" element={<Create />} />
         <Route path="/update" element={<Update />} />
         <Route path="/search" element={<Search />} />
       </Routes>
