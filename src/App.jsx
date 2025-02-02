751595cbea2b94a9e0bf38b7d2ed20cc7aa23e4b
Ilya Urchenko <129249660+IlyaUrchenko1@users.noreply.github.com>
2025-02-02 21:07:51 +0300

    Откат

--- src/App.jsx
+++ src/App.jsx
@@ -4,20 +4,8 @@ import Create from './components/Create/Create'
 import Search from './components/Search/Search'
 import Update from './components/Update/Update'
 import Default from './components/Default/Default'
-import { useEffect } from 'react'
 
 function App() {
-  useEffect(() => {
-    const isWebApp = window.matchMedia('(display-mode: standalone)').matches || 
-                     window.navigator.standalone || 
-                     document.referrer.includes('android-app://');
-                     
-    if (!isWebApp) {
-      document.body.innerHTML = '<h1>Это приложение доступно только как веб-приложение</h1>';
-      throw new Error('Приложение должно быть установлено как веб-приложение');
-    }
-  }, []);
-
   return (
     <Router>
       <Routes>
